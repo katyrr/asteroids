9a48ecb81b80169c2a976c8f33be4778ca09fbd2
katyrr <krrobson9@gmail.com>
2025-08-10 10:06:57 +0100

    feat: limit shoot speed

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0.0
 
     # in the player class
     def triangle(self):
@@ -30,6 +31,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
+        self.shot_timer -= dt
+        
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -48,6 +51,10 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
+        if self.shot_timer > 0.0:
+            return
+        
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation)
         shot.velocity *= PLAYER_SHOOT_SPEED
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
