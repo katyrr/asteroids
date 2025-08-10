ad2da41045db2eaabe9c6b09b70758e4b748712b
katyrr <krrobson9@gmail.com>
2025-08-10 09:32:55 +0100

    feat: draw asteroids

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+from CircleShape import CircleShape
+import pygame
+from constants import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, WHITE, (self.position.x, self.position.y), self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from astroid_field import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -14,12 +16,15 @@ def main():
 
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
-    print(updatables)
-    print(drawables)
-
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    print(updatables)
+    asteroid_field = AsteroidField()
+
     print(drawables)
 
     # infinite game loop:
